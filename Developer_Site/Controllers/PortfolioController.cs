55e07e1096db47c9f13f0bade8e0a0124b942c53
Thomas <azoskalias@gmail.com>
2024-01-09 20:00:41 -0500

    Initial Commit - Portfolio Site

---
+++ Developer_Site/Controllers/PortfolioController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Developer_Site.Models;
+
+namespace Developer_Site.Controllers;
+
+
+public class PortfolioController : Controller
+{
+    private readonly ILogger<PortfolioController> _logger;
+
+    public PortfolioController(ILogger<PortfolioController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public IActionResult Projects()
+    {
+        return View("Views/Portfolio/Projects.cshtml");
+    }
+
+
+    public IActionResult About()
+    {
+        return View("Views/Portfolio/About.cshtml");
+    }
+
+
+    public IActionResult Contact()
+    {
+        return View("Views/Portfolio/Contact.cshtml");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
